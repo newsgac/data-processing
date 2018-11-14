766d07c72430dd3e8dbaf26095b26c6d7ea3fc0c
Erik Tjong Kim Sang <erikt@xs4all.nl>
2018-11-14 17:26:00 +0100

    renamed variables

--- getArticleText.py
+++ getArticleText.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3 -W all
 """
     getArticleText.py: retrieve text of all newspaper articles on a page
-    usage: getArticleText.py < file
+    usage: getArticleText.py [newspaper date page] < file
     note: expects db.txt format: tab separated with dates like d-m-y
     20180416 erikt(at)xs4all.nl
 """
@@ -17,7 +17,7 @@ import xml.etree.ElementTree as ET
 ARTICLEBASEPATH = "./{http://www.loc.gov/zing/srw/}records/{http://www.loc.gov/zing/srw/}record/{http://www.loc.gov/zing/srw/}recordData"
 ARTICLEURLPATH = "./{http://purl.org/dc/elements/1.1/}identifier"
 ARTICLETYPEPATH = "./{http://purl.org/dc/elements/1.1/}type"
-AD = "advertentie"
+ADVERTISEMENT = "advertentie"
 ARTICLE = "artikel"
 ILLUSTRATION = "illustratie met onderschrift"
 COMMAND = sys.argv.pop(0)
@@ -33,30 +33,36 @@ ppns = { "00Algemeen Handelsblad":"400374129",
          "05NRC Handelsblad":"400367629", 
          "06De Telegraaf":"832675288", 
          "07De Maasbode":"842126635", 
-         "08De Volkskrant":"412869594" }
+         "08De Volkskrant":"412869594"}
+titles = { "AH":"00Algemeen Handelsblad", 
+           "NRC":"05NRC Handelsblad", 
+           "Telegraaf":"06De Telegraaf", 
+           "Maasbode":"07De Maasbode", 
+           "Volkskrant":"08De Volkskrant"}
 
-def makeDateId(newspaper,date,pageNbr): 
+def makeDatePageId(newspaper,date,pageNbr):
+    if newspaper in titles: newspaper = titles[newspaper]
     return(newspaper+SEPARATOR+date+SEPARATOR+pageNbr)
 
-def splitDateId(date):
+def splitDatePageId(date):
     return(date.split(SEPARATOR))
 
-def checkDate(dateId):
-    newspaper,date,pageNbr = splitDateId(dateId)
+def checkDate(datePageId):
+    newspaper,date,pageNbr = splitDatePageId(datePageId)
     try: day,month,year = date.split("-")
-    except: sys.exit(COMMAND+": error processing date "+date)
+    except Exception as e: sys.exit(COMMAND+": error processing date "+date+": "+str(e))
     return(int(year) <= MAXYEAR)
 
 def readDBFile():
-    dateIds = {}
+    datePageIds = {}
     csvReader = csv.DictReader(sys.stdin,delimiter=SEPARATOR)
     lineNbr = 0
     for row in csvReader:
         lineNbr += 1
-        try: dateId = makeDateId(row["Titel krant"],row["Datum"],row["Paginanummer"])
+        try: datePageId = makeDatePageId(row["Titel krant"],row["Datum"],row["Paginanummer"])
         except: sys.exit(COMMAND+": missing data on line "+str(lineNbr))
-        if checkDate(dateId): dateIds[dateId] = True
-    return(dateIds)
+        if checkDate(datePageId): datePageIds[datePageId] = True
+    return(datePageIds)
 
 def convertDate(date):
     try: day,month,year = date.split("-")
@@ -66,7 +72,7 @@ def convertDate(date):
     return(year+month+day)
 
 def makeUrl(date):
-    newspaper,date,pageNbr = splitDateId(date)
+    newspaper,date,pageNbr = splitDatePageId(date)
     url = URLPREFIX+str(pageNbr)+URLINFIX1+convertDate(date)+URLINFIX2+ppns[newspaper]+URLPOSTFIX
     return(url)
 
@@ -75,15 +81,15 @@ def getUrlData(url):
     time.sleep(1)
     return(str(urlopen(url,data=None).read(),encoding="utf-8"))
 
-def getArticleUrls(dateId):
-    url = makeUrl(dateId)
+def getArticleUrls(datePageId):
+    url = makeUrl(datePageId)
     htmlData = getUrlData(url)
     root = ET.fromstring(htmlData)
     articleUrls = []
     for article in root.findall(ARTICLEBASEPATH):
         for articleUrl in article.findall(ARTICLEURLPATH):
             articleType = article.findall(ARTICLETYPEPATH)[0]
-            if articleType.text != AD:
+            if articleType.text != ADVERTISEMENT:
                 articleUrls.append(articleUrl.text)
     return(articleUrls)
 
@@ -93,13 +99,13 @@ def getArticleTexts(articleUrls):
         articleTexts.append(getUrlData(articleUrl))
     return(articleTexts)
 
-def makeFileName(dateId):
-    newspaper,date,pageNbr = splitDateId(dateId)
-    dateId = makeDateId(newspaper,convertDate(date),pageNbr)
-    return(re.sub(r"\s",r"-",dateId)+".xml")
+def makeFileName(datePageId):
+    newspaper,date,pageNbr = splitDatePageId(datePageId)
+    datePageId = makeDatePageId(newspaper,convertDate(date),pageNbr)
+    return(re.sub(r"\s",r"-",datePageId)+".xml")
 
-def storeArticleTexts(dateId,articleUrls,articleTexts):
-    fileName = makeFileName(dateId)
+def storeArticleTexts(datePageId,articleUrls,articleTexts):
+    fileName = makeFileName(datePageId)
     with open(fileName,"w") as outFile:
         outFile.write("<container>")
         for i in range(0,len(articleTexts)):
@@ -111,18 +117,18 @@ def storeArticleTexts(dateId,articleUrls,articleTexts):
     return()
 
 def main(argv):
-    dateIds = readDBFile()
+    datePageIds = readDBFile()
     if len(argv) == 3:
-        dateIds[makeDateId(argv[0],argv[1],argv[2])] = True
-    for dateId in dateIds:
-        fileName = makeFileName(dateId)
+        datePageIds[makeDatePageId(argv[0],argv[1],argv[2])] = True
+    for datePageId in datePageIds:
+        fileName = makeFileName(datePageId)
         if not os.path.isfile(fileName):
-            print("Fetching",re.sub(r"\t",r"#",dateId),"...")
-            articleUrls = getArticleUrls(dateId)
+            print("Fetching",re.sub(r"\t",r"#",datePageId),"...")
+            articleUrls = getArticleUrls(datePageId)
             articleTexts = getArticleTexts(articleUrls)
-            storeArticleTexts(dateId,articleUrls,articleTexts)
+            storeArticleTexts(datePageId,articleUrls,articleTexts)
         else:
-            print(re.sub(r"\t",r"#",dateId),"is already in the text collection")
+            print(re.sub(r"\t",r"#",datePageId),"is already in the text collection")
     return(0)
 
 if __name__ == "__main__":
